ec0ca847c1157341ef8dcc50d13ef610db471f90
quythuong <quythuong001@gmail.com>
2024-04-13 09:19:28 +0700

    first commit

--- App.tsx
+++ App.tsx
@@ -5,7 +5,7 @@
  * @format
  */
 
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   SafeAreaView,
@@ -13,6 +13,7 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TouchableHighlight,
   useColorScheme,
   View,
 } from 'react-native';
@@ -25,94 +26,141 @@ import {
   ReloadInstructions,
 } from 'react-native/Libraries/NewAppScreen';
 
-type SectionProps = PropsWithChildren<{
-  title: string;
-}>;
 
-function Section({children, title}: SectionProps): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+
+function App(): React.JSX.Element {
+  const [isRunning, setIsRunning] = useState(false);
+  const [elapsedTime, setElapsedTime] = useState(0);
+  const [laps, setLaps] = useState<number[]>([]);
+  const [countLap, setCountLap] = useState(0)
+  let interval: NodeJS.Timeout;
+
+  useEffect(() => {
+    if (isRunning) {
+      interval = setInterval(() => {
+        setElapsedTime(prevElapsedTime => prevElapsedTime + 0.1);
+      }, 100);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isRunning]);
+
+  const handleStartStop = () => {
+    setIsRunning(prevIsRunning => !prevIsRunning);
+  };
+
+  const handleReset = () => {
+    setIsRunning(false);
+    setElapsedTime(0);
+    setLaps([]);
+  };
+
+  const handleRecordLap = () => {
+    setLaps(prevLaps => [...prevLaps, elapsedTime]);
+  };
+
+  const formatTime = (timeInSeconds: number): string => {
+    const minutes = Math.floor(timeInSeconds / 60).toString().padStart(2, '0');
+    const seconds = Math.floor(timeInSeconds % 60).toString().padStart(2, '0');
+    const milliseconds = Math.floor((timeInSeconds * 100) % 100).toString().padStart(2, '0');
+    return `${minutes}:${seconds}:${milliseconds}`;
+  };
   return (
-    <View style={styles.sectionContainer}>
-      <Text
-        style={[
-          styles.sectionTitle,
-          {
-            color: isDarkMode ? Colors.white : Colors.black,
-          },
-        ]}>
-        {title}
-      </Text>
-      <Text
-        style={[
-          styles.sectionDescription,
-          {
-            color: isDarkMode ? Colors.light : Colors.dark,
-          },
-        ]}>
-        {children}
-      </Text>
+    <View style={styles.container}>
+      <View style={styles.timeWrapper}>
+        <Text style={styles.timer}>
+          {formatTime(elapsedTime)}
+        </Text>
+      </View>
+      <View style={styles.buttonWrapper}>
+        <TouchableHighlight style={styles.LapBtn} underlayColor='gray' onPress={isRunning? handleRecordLap: handleReset}>
+          <Text>{elapsedTime==0? "Lap": (isRunning? "Lap": "Reset")}</Text>
+        </TouchableHighlight>
+        <TouchableHighlight style={[styles.StartBtn, 
+          isRunning? styles.StopBtn : styles.StartBtn
+        ]} underlayColor='gray' onPress={handleStartStop}>
+          <Text>{isRunning? "Stop" : "Start"}</Text>
+        </TouchableHighlight>
+      </View>
+      <ScrollView style={styles.footer}>
+      {laps.map((lap, index) => (
+            <View key={index} style={styles.lap}>
+              <Text style={styles.lapText}>Lap {index + 1}:</Text>
+              <Text style={styles.lapText}>{formatTime(lap)}</Text>
+            </View>
+        ))}
+      </ScrollView>
     </View>
   );
 }
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
-
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
-
-  return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
-      />
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-        style={backgroundStyle}>
-        <Header />
-        <View
-          style={{
-            backgroundColor: isDarkMode ? Colors.black : Colors.white,
-          }}>
-          <Section title="Step One">
-            Edit <Text style={styles.highlight}>App.tsx</Text> to change this
-            screen and then come back to see your edits.
-          </Section>
-          <Section title="See Your Changes">
-            <ReloadInstructions />
-          </Section>
-          <Section title="Debug">
-            <DebugInstructions />
-          </Section>
-          <Section title="Learn More">
-            Read the docs to discover what to do next:
-          </Section>
-          <LearnMoreLinks />
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
-}
-
 const styles = StyleSheet.create({
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
+  container: {
+    flex: 2,
+    margin: 40,
   },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
+  timeWrapper: {
+    flex: 5,
+    justifyContent: 'center',
+    alignItems: 'center'
   },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
+  buttonWrapper: {
+    flex: 2,
+    flexDirection: 'row',
+    justifyContent: 'space-around',
+    alignItems: 'center'
   },
-  highlight: {
-    fontWeight: '700',
+  timer: {
+    fontSize: 60,
   },
+  lap: {
+    justifyContent: 'space-around', 
+    flexDirection: 'row',
+    backgroundColor: 'lightgray',
+    padding: 10,
+    marginTop: 10
+  },
+  lapBtn: {
+    justifyContent: 'space-around', 
+    flexDirection: 'row',
+    backgroundColor: 'lightgray',
+    padding: 10,
+    marginTop: 10
+  },
+  lapText: {
+    fontSize: 30
+  },
+  LapBtn: {
+    borderWidth: 2,
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  StartBtn: {
+    borderWidth: 2,
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#6cf577'
+  },
+  StopBtn: {
+    borderWidth: 2,
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f56464'
+  },
+  footer: {
+    flexDirection: 'column-reverse',
+    height: '30%'
+  }
 });
 
 export default App;
